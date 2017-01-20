d3cde09f0cecdaef03ab8d0f3b8417633455cef6
Yu-Chih Tung <yctung@umich.edu>
2017-01-20 16:44:01 -0500

    add Android libary files

---
+++ Android/LibAcousitcSensing/libacousticsensing/src/main/java/umich/cse/yctung/libacousticsensing/MyLog.java
@@ -0,0 +1,12 @@
+package umich.cse.yctung.libacousticsensing;
+
+import android.util.Log;
+
+/**
+ * Created by eddyxd on 1/20/17.
+ */
+public class MyLog {
+    public void showLog(String msg) {
+        Log.d("MyLog",msg);
+    }
+}
