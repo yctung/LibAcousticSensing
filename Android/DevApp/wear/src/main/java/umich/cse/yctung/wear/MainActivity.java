ae06cd2fee37d39754ec5b9702aa4e993852ec72
Yu-Chih Tung <yctung@umich.edu>
2017-04-12 15:27:55 -0400

    android wear is ok to talk to my matlab server now

---
+++ Android/DevApp/wear/src/main/java/umich/cse/yctung/wear/MainActivity.java
@@ -0,0 +1,125 @@
+package umich.cse.yctung.wear;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.wearable.activity.WearableActivity;
+import android.support.wearable.view.BoxInsetLayout;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import umich.cse.yctung.libacousticsensing.AcousticSensingController;
+import umich.cse.yctung.libacousticsensing.AcousticSensingControllerListener;
+
+
+public class MainActivity extends WearableActivity implements AcousticSensingControllerListener {
+
+    private static final SimpleDateFormat AMBIENT_DATE_FORMAT =
+            new SimpleDateFormat("HH:mm", Locale.US);
+
+    private BoxInsetLayout mContainerView;
+    private TextView mTextView;
+    private TextView mClockView;
+    private Button buttonStart;
+
+    AcousticSensingController asc;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setAmbientEnabled();
+
+        mContainerView = (BoxInsetLayout) findViewById(R.id.container);
+        mTextView = (TextView) findViewById(R.id.text);
+        mClockView = (TextView) findViewById(R.id.clock);
+
+        buttonStart = (Button) findViewById(R.id.buttonStart);
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onStartOrStopClicked();
+            }
+        });
+
+
+        asc=new AcousticSensingController(this,this);
+    }
+
+    void onStartOrStopClicked() {
+        /*
+        Dialog dialog = asc.createInitModeDialog(this, "35.2.141.147", 50005);
+        dialog.show();
+        */
+        boolean result = asc.initAsSlaveMode("10.0.0.12",50005);
+        if (!result) asc.updateDebugStatus("Init fails");
+        else {
+            asc.startSensingWhenPossible();
+        }
+    }
+
+    @Override
+    public void onEnterAmbient(Bundle ambientDetails) {
+        super.onEnterAmbient(ambientDetails);
+        updateDisplay();
+    }
+
+    @Override
+    public void onUpdateAmbient() {
+        super.onUpdateAmbient();
+        updateDisplay();
+    }
+
+    @Override
+    public void onExitAmbient() {
+        updateDisplay();
+        super.onExitAmbient();
+    }
+
+    private void updateDisplay() {
+        if (isAmbient()) {
+            mContainerView.setBackgroundColor(getResources().getColor(android.R.color.black));
+            mTextView.setTextColor(getResources().getColor(android.R.color.white));
+            mClockView.setVisibility(View.VISIBLE);
+
+            mClockView.setText(AMBIENT_DATE_FORMAT.format(new Date()));
+        } else {
+            mContainerView.setBackground(null);
+            mTextView.setTextColor(getResources().getColor(android.R.color.black));
+            mClockView.setVisibility(View.GONE);
+        }
+    }
+
+//=================================================================================================
+//  Acoustic sensing callbacks
+//=================================================================================================
+    @Override
+    public void updateDebugStatus(final String stringToShow) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mTextView.setText(stringToShow);
+            }
+        });
+    }
+
+    @Override
+    public void showToast(String stringToShow) {
+
+    }
+
+    @Override
+    public void sensingEnd() {
+
+    }
+
+    @Override
+    public void updateSensingProgress(int percent) {
+
+    }
+
+}
