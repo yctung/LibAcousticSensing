09aa998a80d43200428bd8fc4898e492f2cb98ca
Arun Ganesan <arunganesan@Aruns-MacBook-Air.local>
2017-09-18 20:33:05 -0400

    added real time tracking. multi peak visualization in real time

--- Matlab/DemoTWatch/PeakCallback.m
+++ Matlab/DemoTWatch/PeakCallback.m
@@ -1,5 +1,12 @@
 function [] =  PeakCallback ( obj, type, recorded_data )
     global PS; % user parse setting
+    global CallbackCounter;
+    global StartTime;
+    
+    
+    %MaxTimePerCallback = duration(0, 0, PS.PERIOD / PS.FS);
+    SecondsPerChirp = seconds(duration(0, 0, PS.PERIOD / PS.FS));
+    
     FIGTAG = "TAG";
     
     if type == obj.CALLBACK_TYPE_ERROR
@@ -11,9 +18,31 @@ function [] =  PeakCallback ( obj, type, recorded_data )
     if type == obj.CALLBACK_TYPE_DATA
         if obj.userfig == -1 % need to create a new UI window
             createUI(obj, FIGTAG);
+            CallbackCounter = -1;
+            StartTime = -1;
         else
+            % If this is the first callback, assume it is on time
+            if CallbackCounter == -1
+                CallbackCounter = 0;
+                StartTime = datetime;
+            else
+                CallbackCounter = CallbackCounter + size(recorded_data, 2);
+            end
+            
+            NowTime = datetime;
+            elapsedInSeconds = seconds(NowTime - StartTime);
+            numChirps = floor(elapsedInSeconds / SecondsPerChirp);
+            
+            %sprintf('Number of chirps we probably have played %d', numChirps)
+            [ CallbackCounter numChirps ]
+            %sprintf('%d/%d', CallbackCounter, numChirps)
+            
+            % Only process this window if we're not lagging too much
+            %if numChirps - CallbackCounter < 20
             drawChannel(1, recorded_data(:,end,1), FIGTAG);
             drawChannel(2, recorded_data(:,end,2), FIGTAG);
+            %end
+            
         end
     elseif type == obj.CALLBACK_TYPE_USER
         % parse user data
@@ -44,10 +73,18 @@ function drawChannel (channelIdx, onechannel, FIGTAG)
     sctplt = findobj('tag',  sprintf('%d-%s-upcorr-scatter', channelIdx, FIGTAG));
     maxpk = findobj('tag', sprintf('%d-%s-upcorr-maxpeak', channelIdx, FIGTAG));
 
-    set(line, 'yData', upcorr(:,end,1));
+    
+    DOWNFACTOR = 10;
+    ONLYTOP = 10;
+    
+    
+    newx = downsample(1:size(upcorr(:,end,1)), DOWNFACTOR);
+    set(line, 'xData', newx);
+    set(line, 'yData', downsample(upcorr(:,end,1), DOWNFACTOR));
     [pks, locs] = findpeaks(upcorr);
-    set(sctplt, 'xData', locs);
-    set(sctplt, 'yData', pks);
+    [topPks, topPksIdx] = sort(pks, 'descend');
+    set(sctplt, 'xData', locs(topPksIdx(1:ONLYTOP)));
+    set(sctplt, 'yData', topPks(1:ONLYTOP));
     [maxPk, maxInd] = max(pks);
     set(maxpk, 'xData', locs(maxInd));
     set(maxpk, 'yData', maxPk);
@@ -56,10 +93,14 @@ function drawChannel (channelIdx, onechannel, FIGTAG)
     sctplt = findobj('tag',  sprintf('%d-%s-downcorr-scatter', channelIdx, FIGTAG));
     maxpk = findobj('tag', sprintf('%d-%s-downcorr-maxpeak', channelIdx, FIGTAG));
 
-    set(line, 'yData', downcorr(:,end,1));
+    set(line, 'xData', newx);
+    set(line, 'yData', downsample(downcorr(:,end,1), DOWNFACTOR));
     [pks, locs] = findpeaks(downcorr);
-    set(sctplt, 'xData', locs);
-    set(sctplt, 'yData', pks);
+    [topPks, topPksIdx] = sort(pks, 'descend');
+    set(sctplt, 'xData', locs(topPksIdx(1:ONLYTOP)));
+    set(sctplt, 'yData', topPks(1:ONLYTOP));
+    %set(sctplt, 'xData', locs);
+    %set(sctplt, 'yData', pks);
     [maxPk, maxInd] = max(pks);
     set(maxpk, 'xData', locs(maxInd));
     set(maxpk, 'yData', maxPk);
@@ -74,7 +115,6 @@ function createUI(obj, figTag)
     
     PADDING = 50;
     WIDTH = 250;
-    BUTTON_HEIGHT = 50;
     
     set(0,'DefaultAxesFontSize',14,'DefaultTextFontSize',16);
     FigPos = [50,50,3*WIDTH+2*PADDING,PADDING*3+WIDTH*2];
