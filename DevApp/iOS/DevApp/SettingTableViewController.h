87bc5314ee0aaffac04f9db40432ac163e523ed8
Yu-Chih Tung <yctung@umich.edu>
2017-10-01 01:55:39 -0400

    [DevApp/iOS] update a workable tableview for setting

---
+++ DevApp/iOS/DevApp/SettingTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  SettingTableViewController.h
+//  DevApp
+//
+//  Created by Yu-Chih on 10/1/17.
+//  Copyright © 2017 Yu-Chih. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingTableViewController : UITableViewController
+
+
+//@property (retain, nonatomic) IBOutlet UITableView *tableView;
+
+@end
