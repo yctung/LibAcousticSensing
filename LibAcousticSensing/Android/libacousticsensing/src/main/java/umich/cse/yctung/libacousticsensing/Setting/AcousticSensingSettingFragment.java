f075d44facd84bbb5ef90ceaa6e5be4e46bc9d0e
Yu-Chih Tung <yctung@umich.edu>
2017-10-02 19:49:33 -0400

    [DevApp/Android]: preference edit screen can show the set value

--- LibAcousticSensing/Android/libacousticsensing/src/main/java/umich/cse/yctung/libacousticsensing/Setting/AcousticSensingSettingFragment.java
+++ LibAcousticSensing/Android/libacousticsensing/src/main/java/umich/cse/yctung/libacousticsensing/Setting/AcousticSensingSettingFragment.java
@@ -4,7 +4,10 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceGroup;
 import android.util.Log;
 
 import umich.cse.yctung.libacousticsensing.R;
@@ -20,17 +23,30 @@ public class AcousticSensingSettingFragment extends PreferenceFragment implement
     @Override
     public void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.preferences);
-        //addPreferencesFromIntent();
     }
 
+    // hack to show the value in the preference edit screen
+    // ref: https://stackoverflow.com/questions/531427/how-do-i-display-the-current-value-of-an-android-preference-in-the-preference-su
     @Override
     public void onResume() {
         super.onResume();
         // Set up a listener whenever a key changes
         getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+
+        for (int i = 0; i < getPreferenceScreen().getPreferenceCount(); ++i) {
+            Preference preference = getPreferenceScreen().getPreference(i);
+            if (preference instanceof PreferenceGroup) {
+                PreferenceGroup preferenceGroup = (PreferenceGroup) preference;
+                for (int j = 0; j < preferenceGroup.getPreferenceCount(); ++j) {
+                    Preference singlePref = preferenceGroup.getPreference(j);
+                    updatePreference(singlePref, singlePref.getKey());
+                }
+            } else {
+                updatePreference(preference, preference.getKey());
+            }
+        }
     }
 
     @Override
@@ -42,16 +58,19 @@ public class AcousticSensingSettingFragment extends PreferenceFragment implement
 
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-        // just update all
-        Log.d(TAG, "key = " + key);
-
-        /*
-        ListPreference lp = (ListPreference) findPreference(PREF_YOUR_KEY);
-        lp.setSummary("dummy"); // required or will not update
-        lp.setSummary(getString(R.string.pref_yourKey) + ": %s");
-         */
-        //SharedPreferences appPref = ((Activity) appContext).getPreferences(Context.MODE_PRIVATE);
-        //appPref.edit().putInt(key, 99999).commit();
-
+        updatePreference(findPreference(key), key);
     }
+
+    private void updatePreference(Preference preference, String key) {
+        if (preference == null) return;
+        if (preference instanceof ListPreference) {
+            ListPreference listPreference = (ListPreference) preference;
+            listPreference.setSummary(listPreference.getEntry());
+            return;
+        }
+        SharedPreferences sharedPrefs = getPreferenceManager().getSharedPreferences();
+        preference.setSummary(sharedPrefs.getString(key, "Default"));
+    }
+
+
 }
