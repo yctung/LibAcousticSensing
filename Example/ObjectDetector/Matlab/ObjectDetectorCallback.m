9402f17b9a7dbe7d6b23ab748cdd009168ac783a
Yu-Chih Tung <yctung@umich.edu>
2017-10-15 13:30:58 -0400

    [Example/ObjectDetection]: clean the callback code

--- Example/ObjectDetector/Matlab/ObjectDetectorCallback.m
+++ Example/ObjectDetector/Matlab/ObjectDetectorCallback.m
@@ -1,7 +1,8 @@
-function [] = AppForcePhoneCallback( obj, type, data )
+function [] = ObjectDetectorCallback( obj, type, data )
 %SERVERDEVCALLBACK Summary of this function goes here
     global USER_FIG_TAG; USER_FIG_TAG = 'USER_FIG_TAG';
     global PS; % user parse setting
+    
     global dsDetectsAll;
     global dsDetectsAllEnd;
     global tvgSetting;
@@ -21,8 +22,9 @@ function [] = AppForcePhoneCallback( obj, type, data )
 
     % parse audio data
     if type == obj.CALLBACK_TYPE_DATA,
-        LINE_CNTS = [2,2,4]; % size of it is the number of figure axes, and the number in it is the number of lines per axe
-        %hFig = findobj('Tag',FIG_CON_TAG);
+%--------------------------------------------------------------------------
+% 1. init GUI
+%--------------------------------------------------------------------------
         if obj.userfig == -1, % need to create a new UI window
             % very large buffer to save all results
             
@@ -46,10 +48,14 @@ function [] = AppForcePhoneCallback( obj, type, data )
             
             
             needToUpdateXLine2 = 1;
+            LINE_CNTS = [2,2]; % size of it is the number of figure axes, and the number in it is the number of lines per axe
             createUI(obj, USER_FIG_TAG, data, LINE_CNTS);
+%--------------------------------------------------------------------------
+% 2. data processing and update figures
+%--------------------------------------------------------------------------
         else
-            % convlution of all data
-            cons = convn(data, PS.signalToCorrelate,'same');
+            % find detected objects by matched filter
+            cons = convn(data, PS.signalToCorrelate, 'same');
             detects = abs(cons(PS.detectRangeStart:PS.detectRangeEnd, :, :));
             traceCnt = size(detects, 2);
             chCnt = size(detects, 3);
@@ -60,7 +66,7 @@ function [] = AppForcePhoneCallback( obj, type, data )
             dsDetectsAll(:, dsDetectsAllEnd+1:dsDetectsAllEnd+traceCnt, 1:chCnt) = dsDetects;
             dsDetectsAllEnd = dsDetectsAllEnd + traceCnt;
             
-            % line1: data 
+            % update line1: data 
             check1 = findobj('Tag','check01');
             if check1.Value == 1,
                 for chIdx = 1:2,
@@ -70,7 +76,7 @@ function [] = AppForcePhoneCallback( obj, type, data )
                 end
             end
 
-            % line2: con 
+            % update line2: detected peaks
             check2 = findobj('Tag','check02');
             if check2.Value == 1,
                 for chIdx = 1:1, % TODO: based on channel
@@ -85,13 +91,12 @@ function [] = AppForcePhoneCallback( obj, type, data )
                     end
                 end
             end
-
-            % line3: detect result
-            check3 = findobj('Tag','check03');
-            if check3.Value == 1,
-                line = findobj('Tag','line03_01');
-            end
         end
+%--------------------------------------------------------------------------
+% 3. handle user data sent from device
+%    in this example, click the user button in device will set a tag to
+%    remember the location
+%--------------------------------------------------------------------------
     elseif type == obj.CALLBACK_TYPE_USER,
         data.code
         % parse user data
@@ -106,6 +111,9 @@ function [] = AppForcePhoneCallback( obj, type, data )
     end
 end
 
+%==========================================================================
+% Followings are just some UI help functions
+%==========================================================================
 function createUI(obj, figTag, data, lineCnts)
     % lineCnts is the number of lines per figure
     global PS;
@@ -114,7 +122,12 @@ function createUI(obj, figTag, data, lineCnts)
     PLOT_AXE_OUT_WIDTH = 290;
     PLOT_AXE_CNT = length(lineCnts);
     
-    obj.userfig = figure('Position',[50,50,550+PLOT_AXE_OUT_WIDTH*(PLOT_AXE_CNT-1),330],'Toolbar','none','MenuBar','none','Tag',figTag);
+    obj.userfig = figure(...
+                    'Name', 'Callback', ...
+                    'Position',[50,50,550+PLOT_AXE_OUT_WIDTH*(PLOT_AXE_CNT-1),330], ...
+                    'Toolbar','none', ...
+                    'MenuBar','none', ...
+                    'Tag',figTag);
     set(obj.userfig,'UserData',obj); % attached the obj to fig property for future reference 
     
     h_panel2 = uipanel(obj.userfig,'Units','pixels','Position',[15,15,520+PLOT_AXE_OUT_WIDTH*(PLOT_AXE_CNT-1),300]);
@@ -126,13 +139,12 @@ function createUI(obj, figTag, data, lineCnts)
     uicontrol(h_panel2, 'Tag','editRangeStart', 'style','edit','units','pixels','position',[80,RANGE_Y,50,30],'String',num2str(PS.detectRangeStart),'FontSize',RANGE_FONT_SIZE);
     uicontrol(h_panel2, 'Tag','editRangeEnd', 'style','edit','units','pixels','position',[130,RANGE_Y,50,30],'String',num2str(PS.detectRangeEnd),'FontSize',RANGE_FONT_SIZE);
     
+    uicontrol(h_panel2,'Style','pushbutton','Position',[30,130,110,30],'String','Apply','Callback',@buttonApplyCallback);
     
-    buttonApply = uicontrol(h_panel2,'Style','pushbutton','Position',[30,130,110,30],'String','Apply','Callback',@buttonApplyCallback);
-
-    uicontrol(h_panel2,'Tag','buttonSqueezeOrStop','Style','pushbutton','Position',[30,90,110,30],'String','Squeeze','Callback',@buttonSqueezeOrStopCallback);
-                    
+    UPDATE_LABELS = {'show recorded signals', 'show detected objects'};
+    X_LABELS = {'sample', 'meter'};
     for i = 1:PLOT_AXE_CNT,
-        uicontrol(h_panel2, 'Style','checkbox','String','update','Value',0,'Position',[220+PLOT_AXE_OUT_WIDTH*(i-1),280,80,20], 'Tag',sprintf('check%02d',i));
+        uicontrol(h_panel2, 'Style','checkbox','String',UPDATE_LABELS{i},'Value',0,'Position',[220+PLOT_AXE_OUT_WIDTH*(i-1),280,200,20], 'Tag',sprintf('check%02d',i));
         
         obj.axe = axes('Parent',h_panel2,'Units','pixels','Position',[220+PLOT_AXE_OUT_WIDTH*(i-1),30,270,250]);
         hold on;
@@ -140,57 +152,10 @@ function createUI(obj, figTag, data, lineCnts)
             plot(obj.axe, data(:,1),'Tag',sprintf('line%02d_%02d',i,j),'linewidth',2); % only show the 1st ch
         end
         hold off;
+        xlabel(X_LABELS{i})
         legend(arrayfun(@(x) sprintf('%d',x), 1:lineCnts(i),'uni',false).');
     end
-        
-
-%MY_SLIDER is an example of Matlab GUI
-% ref: https://www.mathworks.com/help/matlab/creating_guis/share-data-among-callbacks.html#bt9p4qp
-%{
-    hfig = figure('Tag',figTag);
-    set(hfig,'UserData',obj);
-    recordButton = uicontrol('Parent', hfig,'Style','pushbutton',...
-             'Tag','recordButton',...
-             'Units','normalized',...
-             'Position',[0.4 0.3 0.2 0.1],...
-             'String','Record',...
-             'Callback',@recordButtonCallback);
-    line = plot(data(:,1),'r','Tag','line1','linewidth',2); % only show the 1st ch
-%}
 end
-
-function callbackStartSensing(obj, event)
-    fprintf('    callbackStartSensing is called\n');
-    global USER_FIG_TAG;
-    userfig = findobj('Tag', USER_FIG_TAG);
-    ss = userfig.UserData;
-    while ss.latestReceivedAction ~= ss.ACTION_INIT,
-        fprintf('    wait socket is connected\n');
-        pause(1.0);
-    end
-    ss.startSensing();
-end
-
-function recordButtonCallback(obj, event)
-    recordButton = findobj('Tag','recordButton');
-    if strcmp(recordButton.String,'Record'), 
-        recordButton.String = 'Stop';
-        hFig = findobj('Tag', 'FIG_CON_TAG');
-        ss = hFig.UserData;
-        ss.startRecord();
-    elseif strcmp(recordButton.String,'Stop'),
-        recordButton.String = 'Record';
-        prompt = {'Enter the trace tag','Enter the path to save'};
-        dlg_title = 'Input';
-        num_lines = 1;
-        defaultans = {'none','Records/'};
-        answer = inputdlg(prompt,dlg_title,num_lines,defaultans);
-        ss.stopRecord(answer(1), answer(2));
-    else
-        fprintf(2,'[ERROR]: undefined recordButton status\n');
-    end
-end
-
 
 % apply ui control to parse value
 function buttonApplyCallback(obj,event)
@@ -207,27 +172,4 @@ function buttonApplyCallback(obj,event)
         PS.detectRangeStart = rangeStart;
         PS.detectRangeEnd = rangeEnd;
     end
-end
-
-function buttonSqueezeOrStopCallback(obj, event)
-    global ss;
-    buttonSqueezeOrStop = findobj('Tag','buttonSqueezeOrStop');
-    
-    fakeEvent.action = ss.ACTION_USER;
-    if strcmp(buttonSqueezeOrStop.String, 'Squeeze') % going to start squeeze
-        fakeEvent.code = 1;
-        buttonSqueezeOrStop.String = 'Stop';
-    else
-        fakeEvent.code = 0;
-        buttonSqueezeOrStop.String = 'Squeeze';
-    end
-    
-    fakeEvent.time = -1;
-    fakeEvent.stamp = -1;
-    fakeEvent.nameBytes = unicode2native('squeeze');
-    fakeEvent.arg0 = -1;
-    fakeEvent.arg1 = -1;
-    dummyJavahandle = -1;
-    
-    onDataCallback(ss, dummyJavahandle, fakeEvent);
 end
