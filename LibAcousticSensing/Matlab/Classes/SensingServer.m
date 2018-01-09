2558159147109d250cbdf83901f06b03e09c7ab2
Yu-Chih Tung <yctung@umich.edu>
2018-01-09 16:47:31 -0500

    disable some java sensing server function to solve the MAC High Sierra problem

--- LibAcousticSensing/Matlab/Classes/SensingServer.m
+++ LibAcousticSensing/Matlab/Classes/SensingServer.m
@@ -136,7 +136,7 @@ classdef SensingServer < handle
             
             % create java sensing server
             obj.jss = edu.umich.cse.yctung.JavaSensingServer.create(port);
-            obj.jss.SHOW_DEBUG_MESSAGE = false;
+            % obj.jss.SHOW_DEBUG_MESSAGE = false; % yctung: temporarly remove it since access java class variable might not be allowed in High Sierra
             set(obj.jss,'OpAcceptCallback',@(~,~)obj.onAcceptCallback);
             set(obj.jss,'OpDataCallback',@(h,e)obj.onDataCallback(h,e));
             %set(obj.jss,'OpDataCallback',@JavaServerOnDataCallback);
